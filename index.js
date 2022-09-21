8b250c3ce6899c37e2d22d4bd7b786592a969e31
JuanGaravito <juang12162002@gmail.com>
2022-09-21 15:35:35 -0500

    Primer Commit de la nueva funcionalidad

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+let inputs = Array.from(document.querySelectorAll(".inputs"))
+let completed = false
+
 window.addEventListener("paste",(e)=>{
-    let inputs = document.querySelectorAll(".inputs")
     let codigo = e.clipboardData.getData("Text")
 
     if(codigo.length > 5){
@@ -12,11 +14,41 @@ window.addEventListener("paste",(e)=>{
    valueInputs(inputs,true,codigo)
 })
 
+
+function recorreInputs(){
+    inputs.some((input,i) => {
+        if(input.value.length < 1){
+            input.select()
+            inputAnterior =  inputs[i-1]
+            return true
+        }
+        input.disabled = true
+        completed = i == inputs.length-1 ? true : false
+    })
+}
+
+window.addEventListener("input",(e)=>{
+    recorreInputs()
+    window.addEventListener("keydown",(e)=>{
+        if(!completed){
+            if(e.key == "Backspace"){
+                if(inputAnterior != undefined){
+                    recorreInputs()
+                    inputAnterior.disabled = false
+                    inputAnterior.value = ""
+                    inputAnterior.select()
+                }
+            }
+        }
+    }) 
+})
+
+
+
 function valueInputs(inputs,disabled,text = undefined) {
     inputs.forEach((ele,i)=>{
         ele.disabled = disabled
         if(text == undefined){
-            ele.value = ""
             return
         }
         ele.value = text[i]
